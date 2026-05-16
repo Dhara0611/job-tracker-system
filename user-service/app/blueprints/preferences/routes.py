00f9c8951aa84f5836118d25eabbdf64bd1610a2
Dhara Deshpande <Dhara@Dharas-MacBook-Air.local>
2026-05-15 18:40:24 -0700

    added logging and environment variables

--- user-service/app/blueprints/preferences/routes.py
+++ user-service/app/blueprints/preferences/routes.py
@@ -3,9 +3,11 @@ from flask_jwt_extended import jwt_required,get_jwt_identity
 from app.blueprints.preferences.services import upsert, get_preferences_service
 from app.validators.preferences_validator import PreferencesSchema
 from app.validators.decorators import validate_schema
+import logging
 
 pref_bp = Blueprint("preferences", __name__)
 
+logger = logging.getLogger(__name__)
 
 @pref_bp.route("/",methods=["POST"])
 @jwt_required()
@@ -26,3 +28,15 @@ def get_preferences():
     user_id = get_jwt_identity()
     response, status_code = get_preferences_service(user_id)
     return jsonify(response), status_code
+
+
+#endpoint to test error handling
+@pref_bp.route("/test-logs", methods=["GET"])
+def test_logs():
+
+    logger.info("This is an INFO log")
+    logger.warning("This is a WARNING log")
+    logger.error("This is an ERROR log")
+    logger.debug("This is a DEBUG log")
+
+    return {"message": "Logs generated"}, 200
--- user-service/app/blueprints/preferences/services.py
+++ user-service/app/blueprints/preferences/services.py
@@ -1,36 +1,56 @@
 from app.models.preferences import Preference
 from app.extensions import db
+import logging
+from app.utils.exceptions import ValidationError
+
+logger = logging.getLogger(__name__)
 
 def upsert(userid, data):
     
-    pref = Preference.query.filter_by(user_id=userid).first()
+    logger.info(f"Upsert is called for user_id={userid}")
+    if not data:
+        raise ValidationError(" At least one preference field must be provided ")
 
-    if not pref:
-        pref = Preference(user_id = userid)
-#only update the fields that are present in the request
-#otherwise if fields are empty they will override the database
+    try:
 
-    if "role" in data:
-        pref.role = data["role"]
+        pref = Preference.query.filter_by(user_id=userid).first()
 
-    if "experience" in data:
-        pref.experience = data["experience"]
+        if not pref:
+            pref = Preference(user_id = userid)
+    #only update the fields that are present in the request
+    #otherwise if fields are empty they will override the database
 
-    if "location" in data:
-        pref.location = data["location"]
+        if "role" in data:
+            pref.role = data["role"]
 
-    if "salary_min" in data:
-        pref.salary_min = data["salary_min"]
+        if "experience" in data:
+            pref.experience = data["experience"]
 
-    if "salary_max" in data:
-        pref.salary_max = data["salary_max"]
+        if "location" in data:
+            pref.location = data["location"]
 
-    db.session.add(pref)
-    db.session.commit()
+        if "salary_min" in data:
+            pref.salary_min = data["salary_min"]
 
-    return{
-        "message" : "Preferences saved successfully"
-    },200
+        if "salary_max" in data:
+            pref.salary_max = data["salary_max"]
+
+        db.session.add(pref)
+
+        logger.info(f"Committing preferences to database")
+        db.session.commit()
+
+        return{
+            "message" : "Preferences saved successfully"
+        },200
+    
+    except Exception as e:
+    #rollback resets DB state safely
+        db.session.rollback()
+        logger.error(f"Error in upsert preferences for user_id = {userid}: {str(e)}")
+        return {
+            "message" : "Internal server error"
+        }, 500
 
 def get_preferences_service(userid):
     pref = Preference.query.filter_by(user_id=userid).first()
