4bfc4945102ce24b8237f07ff2cb0e96c26816a5
Dhara Deshpande <Dhara@Dharas-MacBook-Air.local>
2026-05-19 16:15:01 -0700

    Finalize job model and database setup

--- job-service/app/__init__.py
+++ job-service/app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask,jsonify
-from app.extensions import db
+from app.extensions import db,migarte
+from app.config import Config
+import app.models
 
 def create_app():
     app = Flask(__name__)
 
+    app.config.from_object(Config)
 
     #initialize database object
     db.init_app(app)
+    migarte.init_app(app,db)
 
     
     @app.route("/")
